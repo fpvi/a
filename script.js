dadac48ce656714f59ee422b3a2f21467031d2d1
fpvi <151766910+fpvi@users.noreply.github.com>
2025-06-28 05:39:00 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+const container = document.getElementById("game-container");
+const instruction = document.getElementById("instruction");
+const closeBtn = document.getElementById("close-btn");
+const bubbleSound = new Audio("sound/bubble-pop.mp3");
+const animals = [
+  "img/angry.png",
+  "img/axolotl (1).png",
+  "img/axolotl.png",
+  "img/eat.png",
+  "img/full.png",
+  "img/happy.png",
+  "img/hi.png",
+  "img/love (1).png",
+  "img/love.png",
+  "img/tired.png",
+];
+
+let lastSpawn = 0;
+const cooldown = 150; // ms
+
+function spawnAnimal(x, y) {
+  const now = Date.now();
+  if (now - lastSpawn < cooldown) return;
+  lastSpawn = now;
+
+  const animal = document.createElement("div");
+  animal.className = "animal";
+
+  const randomAnimal = animals[Math.floor(Math.random() * animals.length)];
+  animal.style.backgroundImage = `url(${randomAnimal})`;
+  animal.style.left = `${x - 40}px`;
+  animal.style.top = `${y - 40}px`;
+
+  container.appendChild(animal);
+  setTimeout(() => animal.remove(), 2000);
+  bubbleSound.currentTime = 0;
+  bubbleSound.play();
+}
+
+// Hide instruction when tap outside
+document.addEventListener("pointerdown", (e) => {
+  if (!instruction.contains(e.target)) {
+    instruction.style.display = "none";
+  }
+
+  spawnAnimal(e.clientX, e.clientY);
+});
+
+container.addEventListener("pointermove", (e) => {
+  if (e.pressure > 0) {
+    spawnAnimal(e.clientX, e.clientY);
+  }
+});
+
+closeBtn.addEventListener("click", () => {
+  instruction.style.display = "none";
+});
